a645191763481eb84a57e9c5a1f4ea4718ce63ca
fotoszezinho10 <fotoszezinho10@gmail.com>
2025-12-19 10:30:39 -0300

    Add files via upload

--- gabinete-virtual/vite.config.ts
+++ gabinete-virtual/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Isso garante que o código geminiService.ts consiga ler a chave da Vercel
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  },
   server: {
     port: 3000,
   },
